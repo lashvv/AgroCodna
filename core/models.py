f65010d26c00625ec6ae09171c51f77f600a2d65
Lasha <lashajinchar.adze16@gmail.com>
2026-08-10 14:07:18 +0400

    Added models for articles and videos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(
+        User,
+        on_delete = models.SET_NULL,
+        null = True,
+        related_name = 'articles'
+    )
+
+class Video(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    video_file = models.FileField(upload_to='videos/')
+    date = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(
+        User,
+        on_delete = models.SET_NULL,
+        null = True,
+        related_name = 'videos'
+    )
